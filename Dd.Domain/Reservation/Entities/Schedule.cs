a854eb32179b21a513afb40d68588884c2e81e3f
Haftom <haftish.tsegay@gmail.com>
2025-08-01 11:08:38 +0300

    feat(entities): identified schedule fields

--- Dd.Domain/Reservation/Entities/Schedule.cs
+++ Dd.Domain/Reservation/Entities/Schedule.cs
@@ -2,4 +2,27 @@ using Dd.Domain.Common.Entities;
 
 namespace Dd.Domain.Reservation.Entities;
 
-public class Schedule : Entity;
+public enum RecurrenceType
+{
+    Daily,
+    Weekly,
+}
+
+public class Schedule : Entity
+{
+    public Physician? Physician { get; set; }
+    public Guid PhysicianId { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    
+    public DateOnly? StartDate { get; set; }
+    public DateOnly? EndDate { get; set; }
+    
+    public bool IsRecurring { get; set; }
+    
+    public RecurrenceType RecurrenceType { get; set; }
+    
+    // if recurring type is weekly
+    public List<DayOfWeek>? RecurrenceDays { get; set; } = [];
+    public int? RecurrenceInterval { get; set; }
+}

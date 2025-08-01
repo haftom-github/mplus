ad037e7bd84da74e7487893794345f9c0be6f874
Haftom <haftish.tsegay@gmail.com>
2025-08-01 10:44:44 +0300

    feat(entities): identified fields of Appointment type

--- Dd.Domain/Reservation/Entities/AppointmentType.cs
+++ Dd.Domain/Reservation/Entities/AppointmentType.cs
@@ -2,4 +2,9 @@ using Dd.Domain.Common.Entities;
 
 namespace Dd.Domain.Reservation.Entities;
 
-public class AppointmentType: Entity;
+public class AppointmentType : Entity
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public TimeSpan Duration { get; set; }
+}

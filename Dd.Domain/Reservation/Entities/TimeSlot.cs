8fb6ca57f55e4e78d3e76c5624d8f5d42c0b4059
Haftom <haftish.tsegay@gmail.com>
2025-08-01 11:46:12 +0300

    feat(entities): identified time slot fields

--- Dd.Domain/Reservation/Entities/TimeSlot.cs
+++ Dd.Domain/Reservation/Entities/TimeSlot.cs
@@ -2,4 +2,12 @@ using Dd.Domain.Common.Entities;
 
 namespace Dd.Domain.Reservation.Entities;
 
-public class TimeSlot : Entity;
+public class TimeSlot : Entity
+{
+    public Physician? Physician { get; set; }
+    public Guid PhysicianId { get; set; }
+    
+    public DateOnly Date { get; set; }
+    public int Index { get; set; }
+    public int Ticks { get; set; }
+}
